1f8d676ea9236d3a3a8ea465e65ce163a39af579
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2026-03-18 17:12:34 +0100

    docs: add docstrings

--- src/Conv.jl
+++ src/Conv.jl
@@ -1,28 +1,38 @@
 """
-KEGGAPI.conv(<target_db>, <source_db>)
+    kegg_conv(target_db::String, source_db::String)
+    kegg_conv(target_db::String, dbentries::String)
 
 Convert KEGG identifiers to/from outside identifiers.
 
-Example:
+# Arguments
+- `target_db::String`: Target database
+- `source_db::String`: Source database
 
-```@example
+# Examples
+```julia
 using KEGGAPI
+
 KEGGAPI.conv("eco", "ncbi-geneid")
 KEGGAPI.conv("ncbi-geneid", "eco")
 KEGGAPI.conv("ncbi-proteinid", "hsa:10458+ece:Z5100")
 KEGGAPI.conv("genes", "ncbi-geneid:948364")
 ```
+
+# Extended help
+
+This operation can be used to convert entry identifiers (accession numbers) of outside databases
+to KEGG identifiers, and vice versa. The first form allows database to database mapping, while
+the second form allows conversion of a selected number of entries. The database name "genes" may
+be used only in the second form.
+
+## References
+
+- [KEGG API](https://www.kegg.jp/kegg/rest/keggapi.html#conv)
+
 """
 function kegg_conv(target_db::String, source_db::String)
-    # This function retrieves a list of entries from a specific database from the KEGG API.
-    # Define the URL for the API request.
     url = "https://rest.kegg.jp/conv/$target_db/$source_db"
     response_text = request(url)
-    kegg_data =
-        conv_parser(
-        response_text,
-        url
-    )
-    # return the arrays
+    kegg_data = conv_parser(response_text, url)
     return kegg_data
 end
